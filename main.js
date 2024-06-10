26e8ac1239d57e630d7c445aa0968b72bac1ad6c
Franko <franko.farhat12@gmail.com>
2024-06-10 10:24:34 +0200

    Update

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 function convertCurrency() {
     const amount = document.querySelector(".amount").value;
     const from = document.querySelector(".fromCurrency").value;
@@ -6,20 +5,17 @@ function convertCurrency() {
     const resultDiv = document.querySelector(".result");
 
     if (amount && from && to) {
-fetch (`https://v6.exchangerate-api.com/v6/4ae059f7f007973c98e5dfe9/latest/${from}`)
-.then((response) => response.json())
-.then((data) => {
-    const rate = data.conversion_rates[to];
-    const result = (amount * rate).toFixed(2);
-    resultDiv.innerHTML =`${amount} ${from} = ${result} ${to}`;
-}).catch((error) => {
-    resultDiv.innerHTML = `Something Went Wrong ${error}`;
-});
+        fetch(`https://v6.exchangerate-api.com/v6/4ae059f7f007973c98e5dfe9/latest/${from}`)
+            .then(response => response.json())
+            .then(data => {
+                const rate = data.conversion_rates[to];
+                const result = (amount * rate).toFixed(2);
+                resultDiv.innerHTML = `${amount} ${from} = ${result} ${to}`;
+            })
+            .catch(error => {
+                resultDiv.innerHTML = `Something went wrong: ${error}`;
+            });
     } else {
-        result.innerHTML = "Please Enter A Amount";
+        resultDiv.innerHTML = "Please enter an amount";
     }
 }
-
-
-
-
